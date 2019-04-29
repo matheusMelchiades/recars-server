d05d1425b6d0bd2acdbb38f42dacd10d535559a4
matheusmelchiades <matheusmacielmelchiades@hotmail.com>
2019-04-29 06:38:47 -0300

    config env to test

--- config/system.js
+++ config/system.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 
 module.exports = (app) => ({
-    authSecret: process.env.AUTH_SECRET || '',
+    authSecret: process.env.AUTH_SECRET || 'secret',
     timeExpirateToken: 60,
     db: {
-        database: process.env.DB_NAME || '',
-        host: process.env.DB_HOST || '',
-        port: process.env.DB_PORT || '',
+        database: process.env.DB_NAME || 'default',
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || '27017',
         user: process.env.DB_USER || '',
         password: process.env.DB_PASSWORD || ''
     },
